fb90e848b8e980c0a3c2c285ed8c99b1cdb7f7c2
mahesh <gangurdemahesh9@gmail.com>
2022-11-23 09:37:35 +0530

    [mahesh]add person details in Table

--- SQL/Day33AddressBook.sql
+++ SQL/Day33AddressBook.sql
@@ -1,1 +1,19 @@
 create database AddressBook
+
+create table addressbook
+(
+FirstName varchar(255),
+LastNames varchar(255), 
+Address varchar(255), 
+City varchar(255),
+State varchar(255),
+Zip int, 
+PhoneNumber varchar(15),
+Email varchar(100)
+)
+
+insert into addressbook(FirstName,LastNames,Address,City,State,Zip,PhoneNumber,Email)
+values
+('mahesh','g','9b nagar','nashik','maharashtra',424008,'9812345678','gagag@gmail.com');
+
+select * from addressbook
